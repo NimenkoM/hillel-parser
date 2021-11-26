d96c706cabbf658f0bd1a0dab17e329f4726227d
NimenkoM <nimenko666@icloud.com>
2021-11-26 23:38:37 +0200

    fixed code

--- parse_url.py
+++ parse_url.py
@@ -1,29 +1,20 @@
-from urllib.parse import urlparse
+from urllib import parse
 
-def url_parser(url):
-    parts = urlparse(url)
-    directories = parts.path.strip('/').split('/')
-    queries = parts.query.strip('&').split('&')
+def url_parser(qeary:str) -> dict:
+    url = "http://www.example.org/default.html?ct=32&op=92&item=98"
+    url = url.split("?")[1]
+    dict = {x[0]: x[1] for x in [x.split("=") for x in url[1:].split("&")]}
 
-    elements = {
-        'scheme': parts.scheme,
-        'netloc': parts.netloc,
-        'path': parts.path,
-        'params': parts.params,
-        'query': parts.query,
-        'fragment': parts.fragment,
-        'directories': directories,
-        'queries': dict(map(lambda x: x.split('='), queries)),
-    }
 
-    return elements
 
-print(url_parser("https://example.com/path/to/page?name=ferret&color=purple"))
-print(url_parser("https://example.com/?name=Dima"))
-print(url_parser("https://example.com/path/to/page?name=ferret&color=purple&"))
-print(url_parser("https://www.youtube.com/watch?v=xRv9Uq1ffbg&list=PLxbd7ySXT5YoTo79ZN4Lo75WADbTDMK4v&index=64&ab_channel=%D0%9C%D0%B8%D1%85%D0%B0%D0%B8%D0%BB%D0%A0%D1%83%D1%81%D0%B0%D0%BA%D0%BE%D0%B2"))
-print(url_parser("https://lms.ithillel.ua/groups/612355f386b02c38d884ecf4/homeworks/61995aa4e1fae77747ae897f"))
+if __name__ == '__main__':
 
+    # Testing function `parse`
+    assert url_parser('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert url_parser('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert url_parser('https://example.com/') == {}
+    assert url_parser('https://example.com/?') == {}
+    assert url_parser('https://example.com/?name=Dima') == {'name': 'Dima'}
 
 
 
