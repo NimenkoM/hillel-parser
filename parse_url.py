c1bdc06a25841c149f578452c9fd729574601aef
NimenkoM <nimenko666@icloud.com>
2021-11-26 23:39:31 +0200

    fixed code

--- parse_url.py
+++ parse_url.py
@@ -1,22 +1,21 @@
 from urllib import parse
-
-def url_parser(qeary:str) -> dict:
-    url = "http://www.example.org/default.html?ct=32&op=92&item=98"
-    url = url.split("?")[1]
-    dict = {x[0]: x[1] for x in [x.split("=") for x in url[1:].split("&")]}
-
-
+def parse_query(query:str) -> dict:
+    return dict(
+        parse.parse_qsl(
+            parse.urlsplit(query).query
+        )
+    )
 
 if __name__ == '__main__':
 
-    # Testing function `parse`
-    assert url_parser('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert url_parser('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert url_parser('https://example.com/') == {}
-    assert url_parser('https://example.com/?') == {}
-    assert url_parser('https://example.com/?name=Dima') == {'name': 'Dima'}
-
-
-
-
+    assert parse_query('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse_query('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse_query('https://example.com/') == {}
+    assert parse_query('https://example.com/?') == {}
+    assert parse_query('https://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse_query('https://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse_query('https://google.com/search?query=JavaScript') == {'query': 'JavaScript'}
+    assert parse_query('http://site.com:8080/path/page?p1=v1&p2=v2') == {'p1': 'v1','p2':'v2'}
+    assert parse_query('http://examples.net/') == {}
+    assert parse_query('https://example.com/path/to/page?name=ferret&color=purple&age=2') == {'name': 'ferret', 'color': 'purple','age':'2'}
 
